9d0d4812ea22441a9d3c61e6c0202f2cf5dddedd
InfernoidPl4y <106786748+infernoidpl4y@users.noreply.github.com>
2026-01-22 23:47:51 -0500

    update

--- solaire.pl
+++ solaire.pl
@@ -19,12 +19,20 @@ sub parser{
 	$code=~s/FUNCION\s+(\w+)\s*\(([^)]*)\)\s*\$\>/sub $1\{\n   my \($2\)=\@_;/g;
 	$code=~s/RETORNAR\s+([^)]+)/return $1/g;
 	$code=~s/\<\$/\}/g;
-	$code=~s/log\(([^)]+)\)/f_log\($1\)/g;
+	$code=~s/\s+ESCRIBIR\s*\(([^)]+)\)/print $1,\"\\n\"/g;
 	$code=~s/SI\s*\(([^)]+)\)\s*\$\>/if\($1\)\{/g;
 	$code=~s/SINO\s*\$\>/else\{/g;
 	$code=~s/PORCADA\s*\(([^)]+)\s+EN\s+([^)]+)\)\s*\$\>/foreach $1 \($2\)\{/g;
 	$code=~s/POR\s*\(([^)]+);([^)]+);([^)]+)\)\s*\$\>/for\($1;$2;$3\)\{/g;
-	$code=~s/ABRIR\s*\(([^)]+)\)/path\($1\)\-\>slurp_utf8/g;
+	$code=~s/a_ABRIR\s*\(([^)]+),UTF8\)/path\($1\)\-\>slurp_utf8/g;
+	$code=~s/a_ABRIR\s*\(([^)]+),ESCL\)/path\($1\)\-\>slurp/g;
+	$code=~s/a_ABRIR\s*\(([^)]+),LISTA\)/path\($1\)\-\>lines/g;
+	$code=~s/a_ESCRIBIR\s*\(([^)]+),([^)]+)\)/path\($1\)\-\>spew\($2\)/g;
+	$code=~s/a_AGREGAR\s*\(([^)]+),\"([^)]+)"\)/path\($1\)\-\>append\(\"\\n$2\"\)/g;
+	$code=~s/a_COPIAR\s*\(([^)]+),([^)]+)\)/path\($1\)\-\>copy\($2\)/g;
+	$code=~s/a_MOVER\s*\(([^)]+),([^)]+)\)/path\($1\)\-\>move\($2\)/g;
+	$code=~s/a_ELIMINAR\s*\(([^)]+)\)/path\($1\)\-\>remove/g;
+	
 	return $code;
 }
 
